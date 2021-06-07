b5fd0e71510431a42ed5a3753eee83882090a348
Sofia Hernandez <a01655084@itesm.mx>
2021-06-07 13:40:46 -0500

    updated time

--- src/app.js
+++ src/app.js
@@ -1,4 +1,24 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `${0[hours]}`;
+    }
+
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `${0[minutes]}`;
+    }
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDate() - 1];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function showTemp(response){
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.name;
 
